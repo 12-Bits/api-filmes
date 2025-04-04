adda94fcf1a9b203f978e060259a92f40ee705e7
12Bits <thelorddosgames@gmail.com>
2025-04-04 16:21:27 -0300

    update styles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,24 +1,57 @@
-import { Container } from "./styles";
+import { Container, MovieList, Movie } from "./styles";
 
 
 function Home(){
+
+    const movies =[
+        {
+            title: 'Spider-Man',
+            image_url:"https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg",
+        },
+        {
+            title: 'Spider-Man',
+            image_url:"https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg",
+        },
+        {
+            title: 'Spider-Man',
+            image_url:"https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg",
+        },
+        {
+            title: 'Spider-Man',
+            image_url:"https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg",
+        },
+        {
+            title: 'Spider-Man',
+            image_url:"https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg",
+        },
+        {
+            title: 'Spider-Man',
+            image_url:"https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg",
+        },
+        {
+            title: 'Spider-Man',
+            image_url:"https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg",
+        },
+        {
+            title: 'Spider-Man',
+            image_url:"https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg",
+        }
+    ]
     return(
         <Container>
         <h1>Movies</h1>
-        <ul>
-            <li>
-                <img src="https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg" alt = "Spider-Man" />
-                <span>Spider Man</span>
-            </li>
-            <li>
-            <img src="https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg" alt = "Spider-Man" />
-            <span>Spider Man</span>
-            </li>
-            <li>
-            <img src="https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg" alt = "Spider-Man" />
-            <span>Spider Man</span>
-            </li>
-        </ul>
+        <MovieList>
+
+        {movies.map(movie => {
+                return (
+                    <Movie>
+                          <a href="https://www.imdb.com/title/tt0110912/" target="_blank" rel="noopener noreferrer"> <img src={movie.image_url} alt = {movie.title} /></a>
+                          <span>{movie.title}</span>
+                    </Movie>
+                )
+            })}
+         
+        </MovieList>
         </Container>
     )
 }
