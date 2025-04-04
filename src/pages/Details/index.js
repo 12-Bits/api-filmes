3b9cea9eb4a7ed2d22a0f547303c8be5168b8f41
12Bits <thelorddosgames@gmail.com>
2025-04-04 18:29:56 -0300

    teste

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,25 +1,46 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { APIKey } from "../../config/key";
-
+import { Container } from "./styles";
 function Details() {
 
     const id = useParams()
-    const {movie, setMovie} = useState({})
+    const [movie, setMovie] = useState({})
+    const image_path = "https://image.tmdb.org/t/p/w500/"
 
 useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language-en-US&page=1`)
+        fetch(`https://api.themoviedb.org/3/movie/${id.id}?api_key=${APIKey}&language-en-US&page=1`)
         .then(response => response.json())
-        .then(data => setMovies(data.results))
+        .then(data => {
+const {title, overview, poster_path, release_date} = data
 
+          const movie = {
+            id,
+            title,
+            sinopse:overview,
+            image: `${image_path}${poster_path}`,
+            ReleaseDate: release_date
+          }
+          setMovie(movie)
 
-}, [id]
-)
+        })
+}, [id])
   return (
-    <div>
-      <h1>Página de Detalhes</h1>
 
-    </div>
+      <Container>
+      <div className="movie">
+        <img src={movie.image} alt={movie.title} />
+        <div className="details">
+          <h1>{movie.title}</h1>
+          <span>{movie.sinopse}</span>
+</div>
+        <div className="RelaseDate">
+        <span>Data de Lançamento: {movie.ReleaseDate}</span>
+        </div>
+      </div>
+
+      </Container>
+
   );
 }
 export default Details;
