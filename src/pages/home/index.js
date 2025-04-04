69ea2f268854c5685af1ddaf55898e5d6ed7eba0
12Bits <thelorddosgames@gmail.com>
2025-04-04 15:50:04 -0300

    teste

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,14 @@
 import { Container } from "./styles";
+
+
 function Home(){
     return(
         <Container>
         <h1>Movies</h1>
         <ul>
             <li>
-<img src="https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg" alt = "Spider-Man" />
-<span>Spider Man</span>
-
+                <img src="https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg" alt = "Spider-Man" />
+                <span>Spider Man</span>
             </li>
             <li>
             <img src="https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg" alt = "Spider-Man" />
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Container = styled.div
+`
 H1{
 text-align: center;
 margin:4rem 0px
@@ -9,5 +10,19 @@ margin:4rem 0px
 ul{
 list-style: none;
 display: grid;
-grid-template-columns: repeat(3, 1fr);}
+grid-template-columns: repeat(auto-fit, minmax(200px, 1fr))
+column-gap: 3rem;
+row-gap: 4rem;
+}
+
+li{
+display: flex;
+flex-direction: column;
+aling-items: center;}
+}
+img{width: 180px;
+border-radius: 1rem;
+margin-bottom: 2rem;}
+
+span{font-weight: bold;}
 `
