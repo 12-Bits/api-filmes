bd1ab403df4d36bac1e79a9e3f78d278b0cad206
12Bits <thelorddosgames@gmail.com>
2025-04-04 15:41:01 -0300

    styles

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,24 @@
+import { Container } from "./styles";
 function Home(){
     return(
-        <div>
+        <Container>
         <h1>Movies</h1>
-        
-        </div>
+        <ul>
+            <li>
+<img src="https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg" alt = "Spider-Man" />
+<span>Spider Man</span>
+
+            </li>
+            <li>
+            <img src="https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg" alt = "Spider-Man" />
+            <span>Spider Man</span>
+            </li>
+            <li>
+            <img src="https://m.media-amazon.com/images/M/MV5BMmFiZGZjMmEtMTA0Ni00MzA2LTljMTYtZGI2MGJmZWYzZTQ2XkEyXkFqcGc@._V1_.jpg" alt = "Spider-Man" />
+            <span>Spider Man</span>
+            </li>
+        </ul>
+        </Container>
     )
 }
 
---
+++ src/pages/home/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+H1{
+text-align: center;
+margin:4rem 0px
+}
+
+ul{
+list-style: none;
+display: grid;
+grid-template-columns: repeat(3, 1fr);}
+`
